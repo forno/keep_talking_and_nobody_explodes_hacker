af37f5634c300529f0cef3fcddc73d1299544801
Doi Yusuke <doi.yusuke806@mail.kyutech.jp>
2019-04-11 19:10:13 +0900

    Add w and hw for wire module

--- src/ktanehack.cpp
+++ src/ktanehack.cpp
@@ -35,7 +35,9 @@ void ktanehack::show_modules(std::ostream& os)
 std::unique_ptr<ktanehack::Module> ktanehack::get_module(std::istream& is, std::ostream& os)
 {
   static const std::unordered_map<std::string, std::function<std::unique_ptr<Module>(std::istream&, std::ostream& os)>> module_map{
-    {"wire", &make_module<WireModule>}
+    {"hw", &make_module<WireModule>},
+    {"w", &make_module<WireModule>},
+    {"wire", &make_module<WireModule>},
   };
 
   auto it{module_map.find(xmaho::input::get_value<std::string>(is))};
