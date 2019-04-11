2610b165af1c1f79dfb4a5f63b6090b48e10e57e
Doi Yusuke <doi.yusuke806@mail.kyutech.jp>
2019-04-11 21:23:51 +0900

    Remove $ for input

--- src/bomb_information.cpp
+++ src/bomb_information.cpp
@@ -16,7 +16,7 @@ bool ktanehack::BombInformation::is_serial_containing_vowel()
     return is_serial_containing_vowel_.value();
   do {
     is_.clear();
-    os_ << "Do SERIAL contains VOEWL? true(1)/false(0)\n$";
+    os_ << "Do SERIAL contains VOEWL? true(1)/false(0)\n";
     is_serial_containing_vowel_ = xmaho::input::get_value<bool>(is_);
   } while (!is_serial_containing_vowel_);
   return is_serial_containing_vowel_.value();
@@ -28,7 +28,7 @@ bool ktanehack::BombInformation::is_serial_last_value_odd()
     return is_serial_last_value_odd_.value();
   do {
     is_.clear();
-    os_ << "Is SERIAL's LAST value ODD? true(1)/false(0)\n$";
+    os_ << "Is SERIAL's LAST value ODD? true(1)/false(0)\n";
     is_serial_last_value_odd_ = xmaho::input::get_value<bool>(is_);
   } while (!is_serial_last_value_odd_);
   return is_serial_last_value_odd_.value();
@@ -40,7 +40,7 @@ int ktanehack::BombInformation::battery_count()
     return battery_count_.value();
   do {
     is_.clear();
-    os_ << "Do SERIAL contains VOEWL? true(1)/false(0)\n$";
+    os_ << "Do SERIAL contains VOEWL? true(1)/false(0)\n";
     battery_count_ = xmaho::input::get_value<int>(is_);
   } while (!battery_count_);
   return battery_count_.value();
