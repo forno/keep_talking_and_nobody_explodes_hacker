0a0751c5d927cb1c878d85617d44f83af81f23b6
FORNO <forno@xmaho.link>
2019-04-19 22:57:22 +0900

    Fix morse code module

--- src/module/morse_code.cpp
+++ src/module/morse_code.cpp
@@ -3,10 +3,34 @@
 #include <iostream>
 #include <iterator>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <valarray>
 
 #include <xmaho/input/input.hpp>
+
+namespace
+{
+
+template<typename T>
+class IncrementGuard
+{
+public:
+  IncrementGuard(T& v)
+    : v_{v}
+  {
+  }
+
+  ~IncrementGuard()
+  {
+    ++v_;
+  }
+
+private:
+  T& v_;
+};
+
+}
 
 void ktanehack::MorseCodeModule::defuse(std::istream& is, std::ostream& os, ktanehack::BombInformation&)
 {
@@ -65,22 +89,38 @@ void ktanehack::MorseCodeModule::defuse(std::istream& is, std::ostream& os, ktan
     const auto it{interpret_map.find(get_value<std::string>(is))};
     if (it == interpret_map.end())
       break;
-    std::valarray<bool> found_flags(answer_map.size());
     base_input.push_back(it->second);
+    std::valarray<bool> found_flags(answer_map.size());
     for (auto i{base_input.size()}; i; --i) {
       std::rotate(base_input.begin(), std::next(base_input.begin()), base_input.end());
-      auto index{0u};
-      for (const auto& e : answer_map) {
-        if (e.first.find(base_input) != std::string::npos)
+      {
+        auto index {0u};
+        for (const auto& e : answer_map) {
+          IncrementGuard{index};
+          if (e.first.find(base_input) != std::string::npos)
+            found_flags[index] = true;
+        }
+      }
+      const std::string_view input_word{base_input};
+      for (auto j{1u}; j < base_input.size(); ++j) {
+        const auto last_words{base_input.substr(j)};
+        const auto forward_words{base_input.substr(0, j)};
+        auto index{0u};
+        for (const auto& e : answer_map) {
+          IncrementGuard{index};
+          if (e.first.find(last_words) != (e.first.size() - last_words.size()))
+            continue;
+          if (e.first.find(forward_words) != 0)
+            continue;
           found_flags[index] = true;
-        ++index;
+        }
       }
     }
     auto index{0u};
     for (const auto& e : answer_map) {
+      IncrementGuard{index};
       if (found_flags[index])
         os << e.first << " : " << e.second << '\n';
-      ++index;
     }
   }
 }
